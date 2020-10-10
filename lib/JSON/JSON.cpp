8ab5dd2c9587fc935d1ecffad45024071e39cc99
Maschine2501 <57831222+Maschine2501@users.noreply.github.com>
2020-10-10 09:05:46 +0200

    Update JSON.cpp

--- lib/JSON/JSON.cpp
+++ lib/JSON/JSON.cpp
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 \*#################################################################*/
 
 #include "JSON.h"
+#include "Arduino.h"
 
 void JSON::stackPush()
 {
